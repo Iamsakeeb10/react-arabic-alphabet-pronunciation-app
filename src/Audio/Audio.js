0a3046ce53b5e21c22c4d61f7c42ac380ea02835
Shakib Ahmed <shakibshovon.10@gmail.com>
2023-07-12 14:19:14 +0600

    changed something

---
+++ src/Audio/Audio.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+import aleef from "../Assets/aleef.mp3";
+import baa from "../Assets/baa.mp3";
+import taa from "../Assets/taa.mp3";
+import cha from "../Assets/cha.mp3";
+import jeem from "../Assets/zeem.mp3";
+import haa from "../Assets/haa.mp3";
+import kho from "../Assets/khoo.mp3";
+import dal from "../Assets/daal.mp3";
+import jal from "../Assets/jaal.mp3";
+import roo from "../Assets/roo.mp3";
+import jha from "../Assets/jhaa.mp3";
+import sin from "../Assets/chin.mp3";
+import shin from "../Assets/shin.mp3";
+import sowat from "../Assets/chowat.mp3";
+import dowat from "../Assets/doyat.mp3";
+import toh from "../Assets/too.mp3";
+import joh from "../Assets/joo.mp3";
+import ayin from "../Assets/ayeen.mp3";
+import goyin from "../Assets/goyeen.mp3";
+import faa from "../Assets/faa.mp3";
+import kohf from "../Assets/kohf.mp3";
+import kahf from "../Assets/kahf.mp3";
+import lam from "../Assets/laam.mp3";
+import mim from "../Assets/meem.mp3";
+import nun from "../Assets/noon.mp3";
+import wow from "../Assets/wow.mp3";
+import haaLast from "../Assets/haalast.mp3";
+import hamza from "../Assets/hamza.mp3";
+import yaa from "../Assets/yaa.mp3";
+
+const Audio = () => {
+  return (
+    <div>
+      <audio src={aleef} id="audio-aleef"></audio>
+      <audio src={baa} id="audio-baa"></audio>
+      <audio src={taa} id="audio-taa"></audio>
+      <audio src={cha} id="audio-cha"></audio>
+      <audio src={jeem} id="audio-jeem"></audio>
+      <audio src={haa} id="audio-haa"></audio>
+      <audio src={kho} id="audio-khoo"></audio>
+      <audio src={dal} id="audio-dal"></audio>
+      <audio src={jal} id="audio-jal"></audio>
+      <audio src={roo} id="audio-raa"></audio>
+      <audio src={jha} id="audio-jha"></audio>
+      <audio src={sin} id="audio-sin"></audio>
+      <audio src={shin} id="audio-shin"></audio>
+      <audio src={sowat} id="audio-sowat"></audio>
+      <audio src={dowat} id="audio-dowat"></audio>
+      <audio src={toh} id="audio-toh"></audio>
+      <audio src={joh} id="audio-joh"></audio>
+      <audio src={ayin} id="audio-ayin"></audio>
+      <audio src={goyin} id="audio-goyin"></audio>
+      <audio src={faa} id="audio-faa"></audio>
+      <audio src={kohf} id="audio-kohf"></audio>
+      <audio src={kahf} id="audio-kahf"></audio>
+      <audio src={lam} id="audio-lam"></audio>
+      <audio src={mim} id="audio-mim"></audio>
+      <audio src={nun} id="audio-nun"></audio>
+      <audio src={wow} id="audio-wow"></audio>
+      <audio src={haaLast} id="audio-last-haa"></audio>
+      <audio src={hamza} id="audio-hamza"></audio>
+      <audio src={yaa} id="audio-yaa"></audio>
+    </div>
+  );
+};
+
+export default Audio;
--- src/UI/Main.js
+++ src/UI/Main.js
@@ -1,70 +1,50 @@
-import React from "react";
 import "./Main.css";
-import Aleef from "../Components/Aleef/Aleef";
-import Baa from "../Components/Baa/Baa";
-import Taa from "../Components/Taa/Taa";
-import Cha from "../Components/Cha/Cha";
-import Zeem from "../Components/Zeem/Zeem";
-import Haa from "../Components/Haa/Haa";
-import Kho from "../Components/Kho/Kho";
-import Dal from "../Components/Dal/Dal";
-import Jaal from "../Components/Jaal/Jaal";
-import Raa from "../Components/Raa/Raa";
-import Jhaa from "../Components/Jhaa/Jhaa";
-import Sin from "../Components/Sin/Sin";
-import Shin from "../Components/Shin/Shin";
-import Soyat from "../Components/Soyat/Soyat";
-import Doyat from "../Components/Doyat/Doyat";
-import Toh from "../Components/Toh/Toh";
-import Joh from "../Components/Joh/Joh";
-import Ayin from "../Components/Ayin/Ayin";
-import Goyin from "../Components/Goyin/Goyin";
-import Faa from "../Components/Faa/Faa";
-import Kohf from "../Components/Kohf/Kohf";
-import Kahf from "../Components/Kahf/Kahf";
-import Laam from "../Components/Laam/Laam";
-import Mim from "../Components/Mim/Mim";
-import Nun from "../Components/Nun/Nun";
-import Wow from "../Components/Wow/Wow";
-import LastHaa from "../Components/LastHaa/LastHaa";
-import Hamza from "../Components/Hamza/Hamza";
-import Yaa from "../Components/Yaa/Yaa";
+import Audio from "../Audio/Audio";
 
 const Main = () => {
+  const audioPlay = (id) => {
+    const audio = document.getElementById(id);
+    audio.play(id);
+    audio.currentTime = 0;
+  };
+
   return (
     <div className="main-container">
       <div className="container">
         <div className="wrapper">
-          <Yaa />
-          <Hamza />
-          <LastHaa />
-          <Wow />
-          <Nun />
-          <Mim />
-          <Laam />
-          <Kahf />
-          <Kohf />
-          <Faa />
-          <Goyin />
-          <Ayin />
-          <Joh />
-          <Toh />
-          <Doyat />
-          <Soyat />
-          <Shin />
-          <Sin />
-          <Jhaa />
-          <Raa />
-          <Jaal />
-          <Dal />
-          <Kho />
-          <Haa />
-          <Zeem />
-          <Cha />
-          <Taa />
-          <Baa />
-          <Aleef />
+          <button onClick={() => audioPlay("audio-yaa")}>ي</button>
+          <button onClick={() => audioPlay("audio-hamza")}>ء</button>
+          <button onClick={() => audioPlay("audio-last-haa")}>ه</button>
+          <button onClick={() => audioPlay("audio-wow")}>و</button>
+          <button onClick={() => audioPlay("audio-nun")}>ن</button>
+          <button onClick={() => audioPlay("audio-mim")}>م</button>
+          <button onClick={() => audioPlay("audio-lam")}>ل</button>
+          <button onClick={() => audioPlay("audio-kahf")}>ك</button>
+          <button onClick={() => audioPlay("audio-kohf")}>ق</button>
+          <button onClick={() => audioPlay("audio-faa")}>ف</button>
+          <button onClick={() => audioPlay("audio-goyin")}>غ</button>
+          <button onClick={() => audioPlay("audio-ayin")}>ع</button>
+          <button onClick={() => audioPlay("audio-joh")}>ظ</button>
+          <button onClick={() => audioPlay("audio-toh")}>ط</button>
+          <button onClick={() => audioPlay("audio-dowat")}>ض</button>
+          <button onClick={() => audioPlay("audio-sowat")}>ص</button>
+          <button onClick={() => audioPlay("audio-shin")}>ش</button>
+          <button onClick={() => audioPlay("audio-sin")}>س</button>
+          <button onClick={() => audioPlay("audio-jha")}>ز</button>
+          <button onClick={() => audioPlay("audio-raa")}>ر</button>
+          <button onClick={() => audioPlay("audio-jal")}>ذ</button>
+          <button onClick={() => audioPlay("audio-dal")}>د</button>
+          <button onClick={() => audioPlay("audio-khoo")}>خ</button>
+          <button onClick={() => audioPlay("audio-haa")}>ح</button>
+          <button onClick={() => audioPlay("audio-jeem")}>ج</button>
+          <button onClick={() => audioPlay("audio-cha")}>ث</button>
+          <button onClick={() => audioPlay("audio-taa")}>ت</button>
+          <button onClick={() => audioPlay("audio-baa")}>ب</button>
+          <button onClick={() => audioPlay("audio-aleef")}>ا</button>
         </div>
+      </div>
+      <div>
+        <Audio />
       </div>
     </div>
   );
